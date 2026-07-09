5dcb0234764d7cad80bb01f3a2a92ddc5bf34bee
Andrii Polonskyi <andriy.polonskiy@gmail.com>
2026-07-09 09:50:50 +0200

    try run in github

--- playwright-project/playwright.config.js
+++ playwright-project/playwright.config.js
@@ -25,9 +25,14 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 3,
+  workers: process.env.CI ? 2 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter:  [['list'], ['html'], ['github'], [
+    '@testomatio/reporter/playwright',
+    {
+      apiKey: process.env.TESTOMATIO,
+    },
+  ]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 60_000,
   expect: {
